be2046da2cc0146c7febca213097ebd770a18187
osaremen <osaremenebhotemen264@gmail.com>
2026-02-11 16:56:13 +0100

    moved images into public folder

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -9,19 +9,19 @@ const HERO_SLIDES = [
         id: 1,
         title: "Lumora Elegance",
         subtitle: "EST. 2026",
-        image: "src/assets/images/patrick-schneider-mFnbFaCIu1I-unsplash (1).jpg"
+        image: "public/images/patrick-schneider-mFnbFaCIu1I-unsplash (1).jpg"
     },
     {
         id: 2,
         title: "Modern Luxe",
         subtitle: "HANDMADE COLLECTION",
-        image: "src/assets/images/hero_background_image.jpg"
+        image: "public/images/hero_background_image.jpg"
     },
     {
         id: 3,
         title: "Bespoke Art",
         subtitle: "CUSTOM DESIGNS",
-        image: "src/assets/images/waldemar-brandt-QxUszVGKt98-unsplash.jpg"
+        image: "public/images/waldemar-brandt-QxUszVGKt98-unsplash.jpg"
     }
 ];
 
